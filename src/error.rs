5d08e5baf0be74e239fbed57722e3d47e77a8f1e
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-10-20 17:56:07 -0500

    feat: impl `Display` and `Error` for error type (#14)

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,22 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum Error {
     InvalidBufferSize,
     Io(std::io::Error),
     Mmap(std::io::Error),
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidBufferSize => write!(f, "invalid buffer size"),
+            Self::Io(err) => write!(f, "io; err={err}"),
+            Self::Mmap(err) => write!(f, "mmap; err={err}"),
+        }
+    }
 }
 
 impl From<std::io::Error> for Error {
