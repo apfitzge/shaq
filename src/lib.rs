412d2ecd60077d8bc62ceb58dc6b8bf9f95a8a40
Andrew Fitzgerald <apfitzge@gmail.com>
2026-02-24 19:08:22 +0000

    Bump VERSION to 2

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub mod mpmc;
 mod shmem;
 pub mod spsc;
 
-pub(crate) const VERSION: u8 = 1;
+pub(crate) const VERSION: u8 = 2;
 
 /// `AtomicUsize` with 64-byte alignment for better performance.
 #[derive(Default)]
